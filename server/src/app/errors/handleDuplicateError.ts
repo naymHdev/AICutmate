205e5b61a6e1e19b393b1bbb92545cd548fb90e5
MacBook Pro <naym100m@gmail.com>
2025-01-21 00:06:26 +0600

    [created] all error handaling middlewares

---
+++ server/src/app/errors/handleDuplicateError.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  TErrorSources,
+  TGenericErrorResponse,
+} from "../interfaces/error.interface";
+
+export const handleDuplicateError = (error: any): TGenericErrorResponse => {
+  const match = error.message.match(/"([^"]*)"/);
+
+  const extractErrorMessage = match && match[1];
+
+  const errorSources: TErrorSources = [
+    {
+      path: "",
+      message: `${extractErrorMessage} <--- Its already exist`,
+    },
+  ];
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: "Invalid request data",
+    errorSources,
+  };
+};
---
+++ server/src/app/errors/handleZodError.ts
@@ -0,0 +1,22 @@
+import { ZodError, ZodIssue } from "zod";
+import {
+  TErrorSources,
+  TGenericErrorResponse,
+} from "../interfaces/error.interface";
+
+export const handleZodError = (error: ZodError): TGenericErrorResponse => {
+  const errorSources: TErrorSources = error.issues.map((issue: ZodIssue) => {
+    return {
+      path: issue.path[issue.path.length - 1],
+      message: issue.message,
+    };
+  });
+
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: "Zod validation error",
+    errorSources,
+  };
+};
